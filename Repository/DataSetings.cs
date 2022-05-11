3606d3026f9fc763cf965148bc7c5e20c7d9deed
КеримханЖ <@223-6>
2022-05-11 18:36:30 +0600

    Добавьте файлы проекта.

---
+++ Repository/DataSetings.cs
@@ -0,0 +1,62 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DataSetings
+    {
+        public string path { get; private set; }
+        MCSEntities db = new MCSEntities();
+
+        public DataSetings() { }
+        public DataSetings(string path)
+        {
+            this.path = path;
+        }
+        
+
+        public List<newEquipment> getListOfCar(DateTime from, DateTime to)
+        {
+            List<newEquipment> newEquipments = new List<newEquipment>();
+            newEquipments = db.newEquipment.
+                Where(w => w.CreateDate >= from && w.CreateDate <= to).ToList();
+            
+            return newEquipments;
+        }
+
+        public bool creatExel(List<newEquipment> newEquipments)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage(new FileInfo(path)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+                int k = 3;
+                foreach(var elem in newEquipments)
+                {
+                    worksheet.Cells[k, 1].Value = elem.TablesModel.strName;
+                    worksheet.Cells[k, 2].Value = "";
+                    worksheet.Cells[k, 3].Value = elem.LastDate;
+                    worksheet.Cells[k, 4].Value = "";
+                    k++;
+                }
+                try
+                {
+                    package.SaveAs("Result_" + DateTime.Now.Year + ".xlsx");
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime from = DateTime.MinValue;
+            DateTime to = DateTime.MinValue;
+            if (DateFrom.SelectedDate != null)
+            {
+                from = (DateTime)DateFrom.SelectedDate;
+            }
+            else
+            {
+                MessageBox.Show("Выберите дату c");
+            }
+            if (DateTo.SelectedDate != null)
+            {
+                to = (DateTime)DateTo.SelectedDate;
+            }
+            else
+            {
+                MessageBox.Show("Выберите дату по");
+            }
+
+            DataSetings setings = new DataSetings(@"C:\Users\ГерценЕ\Downloads\OverallRepairList.xlsx");
+
+            List<newEquipment> newEquipments = setings.getListOfCar(from,to);
+            if (!newEquipments.Any())
+            {
+                MessageBox.Show("Даных по выбраным пораметрам не найдено");
+            }
+            else
+            {
+
+                if (setings.creatExel(newEquipments))
+                {
+                    MessageBox.Show("Файл успешно создан");
+                }
+                else
+                {
+                    MessageBox.Show("Файл не создан");
+                }
+
+            }
+        }
+    }
+}
